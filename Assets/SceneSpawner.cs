e4993f4f36cf43c08a01ff59fcf1e878e3e0853a
Dylan Dinio <DDinio2101@gmail.com>
2022-03-26 02:38:44 -0400

    Fixing merges

--- Assets/SceneSpawner.cs
+++ Assets/SceneSpawner.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 public class SceneSpawner : MonoBehaviour
 {
     public GameObject background;
-    public GameObject thisObject;
+    public GameObject baseGround;
     private bool shouldSpawn = true;
     public ParticleSystem burst;
     public float floatTime;
+    private Vector3 startPosition;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         this.background.SetActive(false);
-    }
+        this.startPosition = this.gameObject.transform.position;
+        this.rb = this.gameObject.GetComponent<Rigidbody>();
+        this.rb.isKinematic = false;
 
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    public IEnumerator disappearAfterSec(float time)
-    {
-        yield return new WaitForSeconds(time);
-
-
-    }
 
     public void floatBurst()
     {
         Invoke("playBurst", this.floatTime);
         Invoke("spawnScene", this.floatTime);
+        Invoke("resetPosition", this.floatTime + 1f);
     }
 
     private void playBurst()
@@ -45,8 +45,12 @@ public class SceneSpawner : MonoBehaviour
         if(this.shouldSpawn == true)
         {
             this.background.SetActive(true);
+            this.baseGround.SetActive(false);
+            this.shouldSpawn = false;
         } else
         {
+            this.background.SetActive(false);
+            this.baseGround.SetActive(true);
             this.shouldSpawn = true;
         }
     }
@@ -55,5 +59,11 @@ public class SceneSpawner : MonoBehaviour
     {
         this.background.SetActive(false);
         this.shouldSpawn = false;
+    }
+
+    public void resetPosition()
+    {
+        this.gameObject.transform.position = startPosition;
+        this.rb.isKinematic = true;
     }
 }
